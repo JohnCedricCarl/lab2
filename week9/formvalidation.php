c88e4afa1a0fc5ff757bbf753045f4f6793810b9
JohnCedricCarl <jbmolina@student.apc.edu.ph>
2023-02-05 15:01:18 +0800

    FORMS_9

---
+++ week9/formvalidation.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <link rel="stylesheet" href="formvalidation.css">
+</head>
+<body>  
+<div class="navbar">
+    <ul>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+        <li><a href="#resources">RESOURCES</a></li>
+        <li><button type="button"
+          onclick="document.getElementById('demo').innerHTML = Date()">
+          What time is it?</button>
+          
+          <p id="demo"></p></li>
+      </ul>
+  </div>
+<?php
+
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>Who u?</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Message: <textarea name="message" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $message;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
